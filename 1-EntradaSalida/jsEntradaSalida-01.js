dfb4cec7ddf625a6a87ccb17201f337ce57cd520
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-01-29 23:14:12 -0300

    Nuevo Cambio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("La última es la vencida");
+	alert("Esto funciona de maravilla");
 }
 
