8018f8581b21dace8041f74d32813465145d0a67
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-01-31 22:17:42 -0300

    Nuevo Cambio 31-1 22:09

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -6,6 +6,6 @@ function mostrar()
 {
 	var nombreIngresado = prompt("Ingrese un ID");
 	//txtIdNombre.value = nombreIngresado; //
-	document.getElementById("txtIdNombre").value = nombreIngresado
+	document.getElementById("txtIdNombre").value = nombreIngresado;
 }
 
