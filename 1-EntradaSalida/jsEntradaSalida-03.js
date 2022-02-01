ef4de9b1e0d52ad4c8c34b25e571c33701334437
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-01-31 21:59:42 -0300

    Nuevo cambio 31-1 21:51

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,7 +3,7 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
+	var nombre;
 	nombre = document.getElementById("txtIdNombre").value;
 	/* nombreIngresado=txtIdNombre.value; Se puede utilizar como opción 
 	pero al hacerlo de este modo, se convierte en variable y en códigos muy largos
