a73127102336103a80efa1aaccc58261caa9fa1d
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-01-31 21:58:26 -0300

    Nuevo cambio 31-1 21:50

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,8 +4,11 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert(txtIdNombre.value);
+	nombre = document.getElementById("txtIdNombre").value;
+	/* nombreIngresado=txtIdNombre.value; Se puede utilizar como opción 
+	pero al hacerlo de este modo, se convierte en variable y en códigos muy largos
+	se puede sobreescribir y generar errores */ 
+	alert(nombre);
 
 }
 
