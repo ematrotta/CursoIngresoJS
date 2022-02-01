cf6f0f48222a2cc1bc1575e4cd526c96dcc05c17
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-01-31 22:32:13 -0300

    Nuevo cambio 31-1 22:24

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,33 +5,33 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	var nroUno = parseInt(txtIdNumeroUno.value);
-	var nroDos = parseInt(txtIdNumeroDos.value);
+	var nroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var nroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
 	var resultado = nroUno + nroDos;
-	alert("La suma es "+ resultado);	
+	alert("La suma es "+ resultado);
 }
 
 function restar()
 {
-	var nroUno = parseInt(txtIdNumeroUno.value);
-	var nroDos = parseInt(txtIdNumeroDos.value);
+	var nroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var nroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
 	var resultado = nroUno - nroDos;
 	alert("La resta es "+ resultado);
 }
 
 function multiplicar()
 { 
-	var nroUno = parseInt(txtIdNumeroUno.value);
-	var nroDos = parseInt(txtIdNumeroDos.value);
+	var nroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var nroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
 	var resultado = nroUno * nroDos;
 	alert("La multiplicación es "+ resultado);
 }
 
 function dividir()
 {
-	var nroUno = parseInt(txtIdNumeroUno.value);
-	var nroDos = parseInt(txtIdNumeroDos.value);
+	var nroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var nroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
 	var resultado = nroUno / nroDos;
-	alert("La división es "+ resultado);
+	alert("La división es "+ resultado);;
 }
 
