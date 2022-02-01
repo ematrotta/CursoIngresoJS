29c041f573d1f887cf0b566bb1d6a7a0449dafdd
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-01-31 23:09:35 -0300

    Nuevo cambio 31-1 23:01

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,11 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var incremento;
 
-	alert("ok");
+	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+	incremento = sueldo * 1.1 ;
+	document.getElementById("txtIdResultado").value = incremento;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,10 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importe;
+	var desc;
+
+	importe = parseInt(document.getElementById("txtIdImporte").value);
+	desc = importe * 0.75 ;
+	document.getElementById("txtIdResultado").value = desc;
 }
