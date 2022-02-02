9bf138d2df2824347f6743129772938c9cef1361
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-02-01 22:21:18 -0300

    Nuevo Cambio 01-02 22:13

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,20 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
-	
+	var edad;
+	var estadoCivil;
+	var estadoCivilSelect;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil");
+	estadoCivilSelect = estadoCivil.options[estadoCivil.selectedIndex].text;
+
+	if (edad >= 18 && estadoCivilSelect == "Soltero")
+	{
+		alert("Es soltero y no es menor");
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
