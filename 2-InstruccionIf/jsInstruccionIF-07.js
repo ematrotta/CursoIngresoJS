c442d3097d8072eb6e113fcde217c18fe0a50dd8
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-02-12 13:07:08 -0300

    Nuevo Cambio 12-02 13:07

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,6 +1,7 @@
 function mostrar()
 {
-	var edad;
+	// EJERCICIO CORRECTO
+	/* var edad;
 	var estadoCivil;
 	var estadoCivilSelect;
 
@@ -12,6 +13,94 @@ function mostrar()
 	{
 		alert("Es muy pequeño para NO ser soltero");
 	}
+	*/
+
+	//CLASE SABADO 12-02
+	
+	var edad;
+	var mensaje;
+	var nombre;
+	var cumplido;
+	var comentarioEstadoCivil;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	mensaje = "";
+	nombre = prompt("Ingrese su nombre").toLowerCase();
+	cumplido = "";
+	estadoCivil = document.getElementById("estadoCivil").value;
+	comentarioEstadoCivil ="";
+
+	if (edad < 13)
+	{
+		if(nombre == "ricardo")
+			{
+				cumplido = ", muy chiquito para ese nombre.";
+			}
+		mensaje = nombre + cumplido + " Feliz día";
+	}
+	else
+	{
+		if(estadoCivil = "Casado")
+		{
+			comentarioEstadoCivil = " Casado, quiere Casa. ";
+		}
+		if(edad < 18)
+		{
+			if(estadoCivil = "Divorciado")
+			{
+				comentarioEstadoCivil = " Toda una vida por delante";
+			}
+			if(nombre == "violeta")
+			{
+				cumplido = ", como un color!!.";
+			}
+			mensaje = nombre + cumplido + " Usted es adolecente";
+			if (edad == 17)
+			{
+				mensaje = mensaje + " , último año!!!";
+			}
+
+		}
+		else
+		{
+			if(edad < 60)
+			{
+				if(estadoCivil = "Soltero")
+				{
+					comentarioEstadoCivil = " A salir.";
+				}
+				mensaje = nombre + " tienes edad de laburar";
+				if(edad == 33)
+				{
+					mensaje = mensaje + ", como Cristo";
+				}
+			}
+			else
+			{
+				if(nombre == "alfredo")
+				{
+					cumplido = ", como el de QUEEN.";
+				}
+				mensaje = nombre + cumplido + " A jubilarse";
+				if(edad == 88)
+				{
+					mensaje = mensaje + ", lindo número";
+				}
+
+			}
+
+
+		}
+
+
+	}
+	if((edad % 2) == 0)
+	{
+		mensaje = mensaje + ", sos par!!";
+	}
+
+	alert(mensaje + comentarioEstadoCivil);
+
 
 
 
