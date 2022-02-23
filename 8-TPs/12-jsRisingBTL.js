59fb58574c8648f2349622d0ed6dfe9ee7b813c8
Emanuel Trotta <emanueltrotta10@gmail.com>
2022-02-22 22:35:15 -0300

    Nuevo Cambio 22-02 22:35

--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -9,10 +9,106 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 	//definicion de variables
- 	var edadIngresada;
- 	var sexoIngresado;
- 	var estadoCivilIngresado;
+
+	//EJERCICIO APLICADO WHILE
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var sueldoBruto;
+	var numeroLegajo;
+	var nacionalidad;
+
+	//1- INGRESO LA EDAD Y LA VAIDO
+	edad = parseInt(prompt("Ingrese la edad"));
+	while(edad < 18 || edad > 90)
+	{
+		edad = parseInt(prompt("Ingrese una edad entre 18 y 90"));
+	}
+	document.getElementById("txtIdEdad").value = edad;
+
+	//2- INGRESO EL SEXO Y LO VALIDO
+	sexo = prompt("Ingrese el sexo: M o F").toLowerCase();
+	while(sexo != "m" && sexo != "f")
+	{
+		sexo = prompt("Para el sexo ingrese solo M o F").toLowerCase();
+	}
+	if(sexo == "f")
+	{
+		sexo = "Femenino";
+	}
+	else
+	{
+		sexo = "Masculino";
+	}
+	document.getElementById("txtIdSexo").value = sexo;
+
+	//3- ESTADO CIVIL Y LO VALIDO
+	estadoCivil = parseInt(prompt("1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos"));
+	while(estadoCivil < 1 || estadoCivil > 4)
+	{
+		estadoCivil = parseInt(prompt("Ingrese un número del 1 al 4"));
+	}
+	switch(estadoCivil)
+	{
+		case 1:
+			estadoCivil = "Soltero";
+			break;
+		case 2:
+			estadoCivil = "Casado";
+			break;
+		case 3:
+			estadoCivil = "Divorciado"
+			break;
+		default:
+			estadoCivil = "Viudo";
+			break;
+	}
+	document.getElementById("txtIdEstadoCivil").value = estadoCivil;
+
+	//4- INGRESO SUELDO BRUTO Y LO VALIDO
+	sueldoBruto = parseInt(prompt("Ingrese el sueldo bruto"));
+	while(sueldoBruto < 8000)
+	{
+		sueldoBruto = parseInt(prompt("El sueldo bruto debe ser mayor a 8000"));
+	}
+	document.getElementById("txtIdSueldo").value = sueldoBruto;
+
+	//5- NUMERO DE LEGAJO VALIDADO
+	numeroLegajo = parseInt(prompt("Ingrese un número de legajo"));
+	while(numeroLegajo < 1000 || numeroLegajo > 10000)
+	{
+		numeroLegajo = parseInt(prompt("Debe contener 4 cifras sin 0 a la izquierda"));
+	}
+	document.getElementById("txtIdLegajo").value = numeroLegajo;
+
+	//6- INGRESO NACIONALIDAD Y VALIDO
+	nacionalidad = prompt("Ingrese nacionalidad: A para argentino, E Extranjeros o N nacionalizados").toLowerCase();
+	while(nacionalidad != "a" && nacionalidad != "e" && nacionalidad != "n")
+	{
+		nacionalidad = prompt("Ingrese solo las letras A, E o N").toLowerCase();
+	}
+	switch(nacionalidad)
+	{
+		case "a":
+			nacionalidad = "Argentino";
+			break;
+		case "e":
+			nacionalidad = "Extrajero";
+			break;
+		case "n":
+			nacionalidad = "Nacionalizado";
+			break;
+	}
+	document.getElementById("txtIdNacionalidad").value = nacionalidad;
+
+
+
+
+
+
+	
+ 
+ 
 
 
 
